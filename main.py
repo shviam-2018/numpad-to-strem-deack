9b56b6341db9fae464e6a4e19ce0a32c12544510
shviam-2018 <122033083+shviam-2018@users.noreply.github.com>
2023-11-25 13:45:41 +0100

    Update main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pynput.keyboard as kb
-from pynput.keyboard import Listener
-
-def on_press(key):
-    if key == kb.Key.num_1:
-        print("Hello, World!")
-
-with Listener(on_press=on_press) as listener:
-    listener.join()
